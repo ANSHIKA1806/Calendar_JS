bb547f37d6f16b72e07a0c32685b34cd7bca245d
ANSHIKA <71966377+ANSHIKA1806@users.noreply.github.com>
2021-05-28 21:11:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,8 +65,8 @@ const renderCalendar = () => {
 
   for (let j = 1; j <= nextDays; j++) {
     days += `<div class="next-date">${j}</div>`;
-    monthDays.innerHTML = days;
-  }
+      }
+  monthDays.innerHTML = days;
 };
 
 document.querySelector(".prev").addEventListener("click", () => {
